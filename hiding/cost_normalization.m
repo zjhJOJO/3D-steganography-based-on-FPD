9e010b8a1bdc6dd2cf419e39cd1625b3d06553d0
FuckingNoO <1158656977@qq.com>
2023-03-22 10:21:08 +0000

    merge conflicts

--- hiding/cost_normalization.m
+++ hiding/cost_normalization.m
@@ -11,7 +11,6 @@ clear; clc; close all;
 %     w=w./max(max(max(w)));
 %     save(fullfile(save_pth,cost(i+2).name),'w');
 % end
-
 cost_pth = '../Cost/costn';
 type = ['cost1','cost2','cost3'];
 cost = dir(fullfile(cost_pth,'cost1'));
@@ -19,18 +18,11 @@ save_pth = '../Cost/costm';
 
 for i=1:length(cost)-2
     name = cost(i+2).name;
-<<<<<<< HEAD
     on=split(name,'.');
-=======
->>>>>>> f169a8e20cb32f9d7dcc4ec987405fbf48f58549
     w1 = load(fullfile(cost_pth,'cost1',name));
     w2 = load(fullfile(cost_pth,'cost2',name));
     w3 = load(fullfile(cost_pth,'cost3',name));
     w1=w1.w;w2=w2.w;w3=w3.w;
     w = w1+w2+w3;
-<<<<<<< HEAD
-    save(fullfile(save_pth,[on{1},'.mat']),'w');
-=======
     save(fullfile(save_pth,'costm.m'),'w');
->>>>>>> f169a8e20cb32f9d7dcc4ec987405fbf48f58549
 end
